404c2501709c61f43863f9960fb08d2f846e0f47
zhangyj <richard.yujia.zhang@gmail.com>
2022-10-13 15:47:00 +0800

    通过Spring Boot Initializr初始化项目

---
+++ nacos-provider/src/main/java/me/zhangrichard/nacosprovider/NacosProviderApplication.java
@@ -0,0 +1,13 @@
+package me.zhangrichard.nacosprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosProviderApplication.class, args);
+	}
+
+}
